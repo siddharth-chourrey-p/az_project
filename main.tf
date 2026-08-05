8482619716f785de2c960c78a9717af67b49088d
siddharth-chourrey-p <sid91chourey@gmail.com>
2026-08-05 22:34:07 +0530

    git actions setup

--- main.tf
+++ main.tf
@@ -2,67 +2,36 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.66.0"
+      version = "~> 4.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg_tfstate"
+    storage_account_name = "dev0tfstate"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = ""
 }
 
-resource "azurerm_resource_group" "dev_rg" {
-  name     = "dev_rg"
-  location = "eastus"
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_resource_group" "stage_rg" {
-  name     = "stage_rg"
-  location = "eastus"
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group."
+  default     = "dev1_rg"
 }
 
-resource "azurerm_resource_group" "prod_rg" {
-  name     = "prod_rg"
-  location = "eastus"
-}
-
-
-resource "azurerm_resource_group" "pre_prod_rg" {
-  name     = "pre_rg"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "stage" {
-  name     = "stage"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "qa" {
-  name     = "qa_rg"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "Dev_rg2" {
-  name     = "Dev_rg2"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "Dev_rrr" {
-  name     = "rrr_rg"
-  location = "eastus"
-}
-resource "azurerm_resource_group" "Dev_rrr1" {
-  name     = "rrr_rg1"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "Dev_rg5" {
-  name     = "Dev_rg5"
-  location = "eastus"
-}
-
-resource "azurerm_resource_group" "uat" {
-  name     = "uat_rg"
-  location = "westus"
+variable "location" {
+  type        = string
+  description = "The Azure region where the resource group should exist."
+  default     = "East US"
 }
